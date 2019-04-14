7740bebd1ce5f03293cdcdcb120044dd092521dd
QiHong Lu <lvqihong1992@gmail.com>
2019-04-14 17:12:21 -0400

    simplify grid world, tested

--- src/GridWorld.py
+++ src/GridWorld.py
@@ -1,106 +1,75 @@
 '''a grid world
-
-# Classes for the Enviroment and the Agent
-
-- The GridWorld class contains the environment
-- The dimensions of the environment are defined
-- Locations of all rewards are stored
-
-- Functions for different methods written
-    - `get_available_actions` returns possible actions
-    - `get_agent_loc` prints out current location of the agent on the grid
-    - `get_reward` returns the reward for an input position
-    - `make_step` moves the agent in a specified direction
-
 '''
 import numpy as np
 
 ACTIONS = ['UP', 'DOWN', 'LEFT', 'RIGHT']
-SIDE_LEN = 5
 
 
 class GridWorld:
     # Initialise starting data
     def __init__(self):
         # Set information about the gridworld
-        self.height = self.width = SIDE_LEN
-        # set default r for all states
-        self.grid = np.zeros((self.height, self.width)) - 1
+        self.height = self.width = 5
         # Set locations for the bomb and the gold
-        self.gold_location = (0, 3)
-        self.bomb_location = (1, 3)
-        self.grid[self.bomb_location] = -10
-        self.grid[self.gold_location] = 10
-        #
-        self.terminal_states = [self.bomb_location, self.gold_location]
+        self.gold_loc = np.array([0, 3])
+        self.bomb_loc = np.array([1, 3])
+        self.terminal_states = [self.bomb_loc, self.gold_loc]
+        # define the state->reward function
+        self.SR_func = np.zeros((self.height, self.width)) - 1
+        self.SR_func[tuple(self.bomb_loc)] = -10
+        self.SR_func[tuple(self.gold_loc)] = 10
+        # init agent location
         self.reset()
 
     def reset(self):
-        self.cur_loc = (SIDE_LEN-1, np.random.randint(0, SIDE_LEN))
+        self.cur_loc = np.array(
+            [self.height-1, np.random.randint(0, self.width)])
 
     def get_agent_loc(self):
         """Prints out current location of the agent on the grid"""
         grid = np.zeros((self.height, self.width))
-        grid[self.cur_loc[0], self.cur_loc[1]] = 1
+        grid[tuple(self.cur_loc)] = 1
         return grid
 
-    def get_reward(self, new_location):
+    def get_reward(self, input_loc):
         """Returns the reward for an input position"""
-        return self.grid[new_location[0], new_location[1]]
+        return self.SR_func[tuple(input_loc)]
 
-    def make_step(self, action):
-        """Moves the agent in the specified direction.
-        If agent is at a border, agent stays still
-        but takes negative reward.
-        Function returns the reward for the move."""
-        # Store previous location
-        prev_loc = self.cur_loc
+    def step(self, action_):
+        """take an action, update the current location of the agent, and
+        return a reward
 
-        # UP
+        Parameters
+        ----------
+        action : int
+            Description of parameter `action`.
+
+        Returns
+        -------
+        type
+            Description of returned object.
+
+        """
+        action = ACTIONS[action_]
         if action == 'UP':
-            # If agent is at the top, stay still, collect reward
-            if prev_loc[0] == 0:
-                reward = self.get_reward(prev_loc)
-            else:
-                self.cur_loc = (self.cur_loc[0] - 1, self.cur_loc[1])
-                reward = self.get_reward(self.cur_loc)
-
-        # DOWN
+            if self.cur_loc[0] != 0:
+                self.cur_loc += [-1, 0]
         elif action == 'DOWN':
-            # If agent is at bottom, stay still, collect reward
-            if prev_loc[0] == self.height - 1:
-                reward = self.get_reward(prev_loc)
-            else:
-                self.cur_loc = (self.cur_loc[0] + 1, self.cur_loc[1])
-                reward = self.get_reward(self.cur_loc)
-
-        # LEFT
+            if self.cur_loc[0] != self.height - 1:
+                self.cur_loc += [1, 0]
         elif action == 'LEFT':
-            # If agent is at the left, stay still, collect reward
-            if prev_loc[1] == 0:
-                reward = self.get_reward(prev_loc)
-            else:
-                self.cur_loc = (self.cur_loc[0], self.cur_loc[1] - 1)
-                reward = self.get_reward(self.cur_loc)
-
-        # RIGHT
+            if self.cur_loc[1] != 0:
+                self.cur_loc += [0, -1]
         elif action == 'RIGHT':
-            # If agent is at the right, stay still, collect reward
-            if prev_loc[1] == self.width - 1:
-                reward = self.get_reward(prev_loc)
-            else:
-                self.cur_loc = (self.cur_loc[0], self.cur_loc[1] + 1)
-                reward = self.get_reward(self.cur_loc)
-
+            if self.cur_loc[1] != self.width - 1:
+                self.cur_loc += [0, 1]
         else:
             raise ValueError(f'unrecognizable action')
-
-        return reward
+        # compute return info
+        r_t = self.get_reward(self.cur_loc)
+        return r_t
 
     def is_terminal(self):
-        """Check if the agent is in a terminal state (gold or bomb),
-        if so return 'TERMINAL'
-        """
-        if self.cur_loc in self.terminal_states:
-            return True
-        return False
+        return np.any([
+            np.all(self.cur_loc == ts) for ts in self.terminal_states
+        ])
--- src/run_qn_agent.py
+++ src/run_qn_agent.py
@@ -38,43 +38,42 @@ for i in range(n_trials):
     game_over = False
 
     while step < max_steps and not game_over:
-        # get an input
+        # get current state
         s_t = to_torch(env.get_agent_loc().reshape(1, -1))
         # compute q val
         q_t = agent.forward(s_t)
-        # a_t_id = torch.argmax(q_t)
+        # epsilon greedy action selection
         if np.random.uniform() > epsilon:
-            a_t_id = torch.argmax(q_t)
+            a_t = torch.argmax(q_t)
         else:
-            a_t_id = np.random.randint(n_actions)
-        a_t = ACTIONS[a_t_id]
+            a_t = np.random.randint(n_actions)
         # transition and get reward
-        r_t = env.make_step(a_t)
+        r_t = env.step(a_t)
+
         # get next states info
-        s_nxt = to_torch(env.get_agent_loc().reshape(1, -1))
-        q_nxt = agent.forward(s_nxt)
-        max_q_nxt = torch.max(q_nxt)
-        # expected q value
-        q_expected = r_t + gamma * max_q_nxt
+        s_next = to_torch(env.get_agent_loc().reshape(1, -1))
+        max_q_next = torch.max(agent.forward(s_next))
+        # compute TD target
+        q_expected = r_t + gamma * max_q_next
 
-        loss = F.smooth_l1_loss(q_t[:, a_t_id], q_expected)
-        # loss = F.mse_loss(q_t, q_nxt)
-        optimizer.zero_grad()
-        loss.backward()
-        for param in agent.parameters():
-            param.grad.data.clamp_(-1, 1)
-        optimizer.step()
+        # loss = F.smooth_l1_loss(q_t[:, a_t], q_expected)
+        # # loss = F.mse_loss(q_t, q_next)
+        # optimizer.zero_grad()
+        # loss.backward()
+        # for param in agent.parameters():
+        #     param.grad.data.clamp_(-1, 1)
+        # optimizer.step()
 
-        # w_delta = alpha * (q_expected) * s_t
-        # agent.linear.weight[a_t_id, :] += torch.squeeze(w_delta)
+        w_delta = alpha * (q_expected) * s_t
+        agent.linear.weight[a_t, :] += torch.squeeze(w_delta)
 
         # update R and n steps
-        cumulative_reward += r_t
         step += 1
+        cumulative_reward += r_t
         epsilon *= epsilon_decay
 
         if env.is_terminal():
-            env.__init__()
+            env.reset()
             game_over = True
 
     log_return.append(cumulative_reward)
@@ -117,21 +116,20 @@ f.tight_layout()
 
 '''show a sample trajectory'''
 
-env.__init__()
+env.reset()
 cumulative_reward = 0
 step = 0
-game_over = False
 locs = []
-while step < max_steps and not game_over:
+while step < max_steps:
     # get an input
     locs.append(env.get_agent_loc())
     s_t = to_torch(locs[step].reshape(1, -1))
     q_t = agent.forward(s_t)
-    r_t = env.make_step(ACTIONS[torch.argmax(q_t)])
+    r_t = env.step(ACTIONS[torch.argmax(q_t)])
     step += 1
     cumulative_reward += r_t
     if env.is_terminal():
-        game_over = True
+        break
 locs.append(env.get_agent_loc())
 step += 1
 
@@ -143,8 +141,8 @@ ax.imshow(
     np.sum([ws[t]*locs[t] for t in range(step)], axis=0),
     cmap='Blues', aspect='auto'
 )
-goal = Circle(env.gold_location[::-1], radius=.1, color='red')
-bomb = Circle(env.bomb_location[::-1], radius=.1, color='black')
+goal = Circle(env.gold_loc[::-1], radius=.1, color='red')
+bomb = Circle(env.bomb_loc[::-1], radius=.1, color='black')
 ax.add_patch(goal)
 ax.add_patch(bomb)
 f.tight_layout()
